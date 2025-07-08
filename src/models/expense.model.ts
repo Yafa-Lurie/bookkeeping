b01fecb340ee19c1a8917f70a109934fc4a130cf
Chevi Binder <chevibinder@gmail.com>
2025-07-08 23:32:53 +0300

    more unknown changes

--- src/models/expense.model.ts
+++ src/models/expense.model.ts
@@ -1,25 +1,27 @@
    import mongoose, { Document, Schema } from 'mongoose';
 
    export interface IExpense extends Document {
-     referenceNumber: string;
+     referenceNumber?: string;
      date: Date;
-     supplier: string; // You can reference a supplier model here
+     supplier?: string; // You can reference a supplier model here
      category: string; // e.g., salary, furniture
      amount: number;
-     vat: number;
-     paymentMethod: string; // e.g., cash, credit
-     reference: string;
+     vat?: number;
+     paymentMethod?: string; // e.g., cash, credit
+     reference?: string;
+     description?: string;
    }
 
    const ExpenseSchema: Schema = new Schema({
-     referenceNumber: { type: String, required: true },
+     referenceNumber: { type: String, required: false },
      date: { type: Date, required: true },
-     supplier: { type: String, required: true },
+     supplier: { type: String, required: false },
      category: { type: String, required: true },
      amount: { type: Number, required: true },
-     vat: { type: Number, required: true },
-     paymentMethod: { type: String, required: true },
-     reference: { type: String }
+     vat: { type: Number, required: false },
+     paymentMethod: { type: String, required: false },
+     reference: { type: String, required: false },
+     description: { type: String, required: false }
    });
 
    export default mongoose.model<IExpense>('Expense', ExpenseSchema);
